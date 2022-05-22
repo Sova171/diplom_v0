e1f643324e7ee61aee71786a3769764e498085d9
Sova171 <vladkuzak@gmail.com>
2022-05-22 23:35:54 +0300

    postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -11,18 +11,21 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_05_16_110257) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "achievements", force: :cascade do |t|
     t.string "title"
     t.text "description"
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_achievements_on_user_id"
   end
 
   create_table "likes", force: :cascade do |t|
-    t.integer "achievement_id", null: false
-    t.integer "user_id", null: false
+    t.bigint "achievement_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["achievement_id"], name: "index_likes_on_achievement_id"
