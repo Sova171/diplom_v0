5e5d123c511aa5179231000630b939acfb565023
Sova171 <vladkuzak@gmail.com>
2022-06-04 16:44:57 +0300

    admin start

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   has_many :achievements, dependent: :destroy
   has_many :likes, dependent: :destroy
-  has_many :likes, dependent: :destroy
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
