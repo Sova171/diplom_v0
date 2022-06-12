f84217bcb60ee18646bbc00a2e9a463f1bd6a9c4
Sova171 <vladkuzak@gmail.com>
2022-06-12 23:53:24 +0300

    rich text for achievements

--- app/models/achievement.rb
+++ app/models/achievement.rb
@@ -2,6 +2,6 @@ class Achievement < ApplicationRecord
   belongs_to :user
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
-
+  has_rich_text :description
   validates :title, length: {minimum: 3}
 end
